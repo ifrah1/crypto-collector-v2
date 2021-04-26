a721b54c9d53a7be0c306abf7679d12014d026b9
ifrah1 <arifrah125@gmail.com>
2021-04-26 18:11:46 -0400

    added purchase model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # import your models here
-from .models import Crypto
+from .models import Crypto, Purchase
 
 # Register your models here
 admin.site.register(Crypto)
+admin.site.register(Purchase)
--- main_app/models.py
+++ main_app/models.py
@@ -17,3 +17,15 @@ class Crypto(models.Model):
     # redirect once a new crypto is created
     def get_absolute_url(self):
         return reverse('detail', kwargs={'crypto_id': self.id})
+
+# purchase model
+class Purchase(models.Model):
+    date = models.DateField('Purchase Date')
+    purchase_price = models.IntegerField()
+    total_amount = models.IntegerField()
+
+    # Create a crypto_id FK 1:M
+    crypto = models.ForeignKey(Crypto, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Bought {self.total_amount} on {self.date}"
