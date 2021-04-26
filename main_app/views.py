dbaf784dafccd4e8ad93433488ec2e3abf760b71
ifrah1 <arifrah125@gmail.com>
2021-04-26 16:48:12 -0400

    finished wk20d1 lab

--- main_app/views.py
+++ main_app/views.py
@@ -5,6 +5,18 @@ from django.http import HttpResponse
 
 from django.shortcuts import render
 
+
+class Crypto:  # Note that parens are optional if not inheriting from another class
+    def __init__(self, name, price, description, amount):
+        self.name = name
+        self.price = price
+        self.description = description
+        self.amount = amount
+
+cryptos = [
+    Crypto('litecoin', 300, 'fast coin', 3),
+    Crypto('bitcoin', 50000, 'OG', 0),
+]
 # Create your views here.
 
 # Define the home view
@@ -14,3 +26,7 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+# Add new view
+def cryptos_index(request):
+    return render(request, 'cryptos/index.html', { 'cryptos': cryptos })
